49763148930852993c0593a9c1ecc68e28040017
r3dgaming12 <45930426+r3dgaming12@users.noreply.github.com>
2018-12-18 15:01:56 +0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("524093203197067285")
+setInterval(function() {  
+channel.send(` R3D Spam bot ya m3lm hhhhhhhhhhh at7dak yl bot alb6l at7daaaaaaaaaak r3d hna #JOOOOOOOOOOOOOOOOOOOOOOOOO hhhhhhh i love r3d hhhh i love you r3d i love you r3d i love you r3d`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
